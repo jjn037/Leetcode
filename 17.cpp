884b87dc8d9d28065d12ce7174b444e9526cb030
姜佳男 <jiangjianan037@126.com>
2018-05-02 08:24:00 -0500

    Create 17.cpp
    17. Letter Combinations of a Phone Number
    回溯新方式

---
+++ 17.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void huisu(string digits, vector<string>& ret, int start, string temp){
+        string num_map[] = {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        if(temp.size()==start ){
+            ret.push_back(temp);
+            return;
+        }
+        for(int i=0; i<num_map[digits[start] - '0'].size(); i++){
+            temp[start] = num_map[digits[start]- '0'][i];
+            huisu(digits, ret, start+1, temp);
+        }
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        if(digits=="") return vector<string>();
+        vector<string> ret;
+        string temp(digits.size(), ' ');
+        huisu(digits, ret, 0, temp);
+        return ret;
+        // map['2'] = "abc"; map['3'] = "def"; map['4'] = "ghi"; map['5'] = "jkl";
+        // map['6'] = "mno"; map['7'] = "pqrs"; map['8'] = "tuv"; map['9'] = "wxyz"; 
+        // for(string::iterator it = s.begin(); it != s.end(); it++){
+        //     string temp = map[*it] ;
+            
+        
+        }
+    };
+
+
